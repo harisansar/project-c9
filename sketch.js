5cd5bd1ebacfaec77ca752ad38a013f5977862b7
harisansar <88089425+harisansar@users.noreply.github.com>
2021-09-05 11:58:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(UP_ARROW))
+  {
+    background('yellow')
+  }
+  if(keyIsDown(DOWN_ARROW))
+  {
+    background('red')
+  }
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    background('blue')
+  }
+  if(keyIsDown(LEFT_ARROW))
+  {
+    background('pink')
+  }
+  drawSprites()
+
+}
+
+
+
+
